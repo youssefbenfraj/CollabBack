23e1aaa345308246b9792a8a481ca48b599eb965
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2024-05-12 19:59:25 +0100

    Update EventRestController.java

--- src/main/java/tn/esprit/espritcollabbackend/restController/EventRestController.java
+++ src/main/java/tn/esprit/espritcollabbackend/restController/EventRestController.java
@@ -32,7 +32,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials="true")// ajoutineha bch el front najmt tchouf el back 5ater ma3ndhech nafs el port w zidna configuration fel security fel crosconfiguration
+@CrossOrigin(origins = "*", maxAge = 3600, allowCredentials="true")// ajoutineha bch el front najmt tchouf el back 5ater ma3ndhech nafs el port w zidna configuration fel security fel crosconfiguration
 @RestController
 public class EventRestController {
     @Autowired
