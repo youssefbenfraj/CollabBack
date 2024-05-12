409eac8370ac4ed04a1854e64364cdc866065fb6
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2024-05-12 19:59:11 +0100

    Update DocumentRestController.java

--- src/main/java/tn/esprit/espritcollabbackend/restController/DocumentRestController.java
+++ src/main/java/tn/esprit/espritcollabbackend/restController/DocumentRestController.java
@@ -8,7 +8,7 @@ import tn.esprit.espritcollabbackend.services.IDocument;
 import tn.esprit.espritcollabbackend.services.IUser;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowCredentials="true")// ajoutineha bch el front najmt tchouf el back 5ater ma3ndhech nafs el port w zidna configuration fel security fel crosconfiguration
+@CrossOrigin(origins = "*", maxAge = 3600, allowCredentials="true")// ajoutineha bch el front najmt tchouf el back 5ater ma3ndhech nafs el port w zidna configuration fel security fel crosconfiguration
 @RestController
 @AllArgsConstructor
 public class DocumentRestController {
